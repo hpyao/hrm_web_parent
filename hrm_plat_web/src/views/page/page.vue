bcef98d3f445eb2322dc4b73a3443e98d2856c8a
dev1 <dev1@163.com>
2019-09-11 20:12:30 +0800

    提交

--- hrm_plat_web/src/views/page/page.vue
+++ hrm_plat_web/src/views/page/page.vue
@@ -88,6 +88,7 @@
 						<el-button size="small" type="primary">点击上传</el-button>
 						<div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
 					</el-upload>
+
 				</el-form-item>
 				<el-form-item label="物理路径" prop="physicalPath">
 					<el-input v-model="pager.physicalPath" auto-complete="off"></el-input>
@@ -138,7 +139,7 @@
                 console.log(response);
                 console.log(file);
                 console.log(fileList);
-                this.tenant.logo = response;
+                this.pager.templateUrl = response;
             },
             handleRemove(file, fileList) {
                 console.log(file, fileList);
